5ed46524fa59264d049ce25de34f113227eb5831
João Baptista <joaosbaptista@hotmail.com>
2018-01-23 03:30:38 -0200

    Update medias.c

--- medias.c
+++ medias.c
@@ -1,18 +1,19 @@
 void medias(const int *A, int n, double *Ma, double *Mg, double *Mrq, double *Mh, double *Dp){
     int i;
+    double mult = 1;
     *Ma = *Mrq = *Mh = *Dp = 0;
-    *Mg = 1; 
+    *Mg = 1.0; 
     for(i = 0; i < n; i++){
         *Ma += *(A+i);
         *Mg *= *(A+i);
-        *Mrq += pow(*(A+i), 2);
-        *Mh += 1/(*(A+i)); 
+        *Mrq += (*(A+i)* (*(A+i)));
+        *Mh += ((1.0)/(*(A+i)));
     }
     *Ma = *Ma/n;
-    *Mg = pow((*Mg), 1/n);
-    *Mrq = pow((*Mrq)/n, 1/2);
-    *Mh = n/(*Mrq);
-    *Dp = pow((n*(*Mrq) - (*Ma*(*Ma)/(n*(n-1)))), 1/2);
+    *Mg = pow(*Mg, (1.0/n));
+    *Mrq = pow((*Mrq)/n, 1.0/2.0);
+    *Mh = n/(*Mh);
+    *Dp = sqrt((n*(*Mrq)) - (*Ma *(*Ma))/n*(n-1));
     
     return;
 
