6d612e9e306c6dbdfe1faf038adf40625c08e4b9
Vinícius Vedovotto <32825138+viniciusveu@users.noreply.github.com>
2018-01-24 01:30:45 -0200

    Add files via upload

--- p2_3.c
+++ p2_3.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 
-void Ordena_Matriz(int *A, int N)
+void Bubble_Sort(int *A, int ini, int N)
 {
-    int i, j, aux, lim=N*N;
-    for(i=0; i<lim; i++) {
-        for(j=i+1; j<lim; j++) {
+    int i, j, aux;
+    for(i=ini; i<N; i++) {
+        for(j=i+1; j<N; j++) {
             if(*(A+j)<*(A+i)) {
                 aux = *(A+j);
                 *(A+j) = *(A+i);
@@ -14,6 +14,25 @@ void Ordena_Matriz(int *A, int N)
             }
         }
     }
+}
+
+
+void Ordena_Linha(int *A, int N)
+{
+    int i;
+    for(i=0; i<=N; i++) {
+        Bubble_Sort(A, i*N, (i+1)*N);
+    }
+    return;
+//    Bubble_Sort(A, 0*N, 1*N);
+//    Bubble_Sort(A, 1*N, 2*N);
+//    Bubble_Sort(A, 2*N, 3*N);
+}
+
+
+void Ordena_Matriz(int *A, int N)
+{
+    Bubble_Sort(A, 0, N*N);
     return;
 }
 
@@ -37,6 +56,13 @@ int main(void)
         printf("\n");
 
     }
+    Ordena_Linha(*mat, dim);
+    printf("\nA matriz com as linhas ordenadas eh: \n");
+    for(i=0; i<dim; i++) {
+        for(j=0; j<dim; j++)
+            printf("[%2d] ", mat[i][j]);
+        printf("\n");
+    }
     Ordena_Matriz(*mat, dim);
     printf("\nA matriz ordenada eh: \n");
     for(i=0; i<dim; i++) {
@@ -44,9 +70,9 @@ int main(void)
             printf("[%2d] ", mat[i][j]);
         printf("\n");
     }
+
     return 0;
 }
-
 
 
 
