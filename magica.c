ba0aa21041394fa4ba26056a1e940dcf626c228b
João Baptista <joaosbaptista@hotmail.com>
2018-01-24 00:03:10 -0200

    magica.c

---
+++ magica.c
@@ -0,0 +1,25 @@
+int verificaMagica(int *A, int n, int k){
+
+    int i, j, somaLin, somaCol, somaDiagPrinc, somaDiagSec,  aux = (2*n) + 2;
+    int list[aux];
+    for(i = 0; i < n; i++){
+        somaLin = 0;
+        somaCol = 0;
+        for(j = 0; j < n; j++){
+            somaLin += *(A+i*n+j);
+            somaCol += *(A+j*n+i);
+            if((i+j) == n-1)
+                somaDiagSec += *(A+i*n+j);
+            if(i == j)
+                somaDiagPrinc += *(A+i*n+j);
+        }
+    }
+
+    printf("Lin %d\nCol %d\nDiagP %d\nDiagSec %d\n", somaLin, somaCol, somaDiagPrinc, somaDiagSec);
+
+    if(somaLin == k && somaCol == k && somaDiagPrinc == k && somaDiagSec == k)
+        return 1;
+    else
+        return 0;
+
+}
